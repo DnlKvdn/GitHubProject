b3baf9aab1ba4055c7174f6fa17099faedca76b4
DnlKvdn <dnl.kvdn@outlook.com>
2021-07-21 18:38:12 +0300

    Commit_NewBranch

--- GitHubProject/GitHubProject.cpp
+++ GitHubProject/GitHubProject.cpp
@@ -7,7 +7,7 @@ using std::cin;
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << " Commit_1!\n";
-
+    std::cout << " Commit_1!_NewBranch\n";
+    std::cout << " Commit_2!_NewBranch\n";
     return 0;
 }
